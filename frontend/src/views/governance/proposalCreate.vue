fae6be284470215e0413fe65d2b7a9685b01c432
sote-dev <github@soteria.finance>
2021-11-19 15:27:04 +0800

    update governance

--- frontend/src/views/governance/proposalCreate.vue
+++ frontend/src/views/governance/proposalCreate.vue
@@ -1,46 +1,50 @@
 <template>
-  <div id="gov-proposal-create" v-loading.fullscreen.lock="loading"
-        element-loading-text="Transaction is confirming ...">
+  <div
+    id="gov-proposal-create"
+    v-loading.fullscreen.lock="loading"
+    element-loading-text="Transaction is confirming ...">
     <el-drawer
       title="Create Proposal"
       :visible.sync="options.showCreate"
       size="50%">
       <div class="create-drawer">
-       <el-form name="createFrm" :model="form" label-width="180px" :rules="rules" ref="createFrm">
-         <el-form-item label="Proposal Title" prop="title">
-           <el-input v-model="form.title" placeholder="Enter a title for proposal"></el-input>
-         </el-form-item>
-         <el-form-item label="Proposal Description" prop="desc">
-           <el-input type="textarea" :rows="5" v-model="form.desc" placeholder="Describe your proposal here"></el-input>
-         </el-form-item>
-       </el-form>
-       <el-button round @click="$refs.createFrm.resetFields()">Reset</el-button>
-       <el-button round type="primary" @click="createProposal">Submit</el-button>
+        <el-form name="createFrm" :model="form" label-width="180px" :rules="rules" ref="createFrm">
+          <el-form-item label="Proposal Title" prop="title">
+            <el-input v-model="form.title" placeholder="Enter a title for proposal"></el-input>
+          </el-form-item>
+          <el-form-item label="Proposal Description" prop="desc">
+            <el-input
+              type="textarea"
+              :rows="5"
+              v-model="form.desc"
+              placeholder="Describe your proposal here"></el-input>
+          </el-form-item>
+        </el-form>
+        <el-button round @click="$refs.createFrm.resetFields()">Reset</el-button>
+        <el-button round type="primary" @click="createProposal">Submit</el-button>
       </div>
     </el-drawer>
   </div>
 </template>
 
 <script>
-import { watch } from '@/utils/watch.js';
-import { mapGetters } from 'vuex';
+import {watch} from '@/utils/watch.js';
+import {mapGetters} from 'vuex';
 import GovernanceContract from '@/services/Governance';
 
 export default {
-  filters: {
-
-  },
-  components: { },
+  filters: {},
+  components: {},
   props: ["options"],
   data() {
     return {
-      form:{
+      form: {
         title: "",
         desc: ""
       },
-      rules:{
-        title: [{ required: true, trigger: 'blur', message: "Proposal title is category."}],
-        desc: [{ required: true, trigger: 'blur', message: "Proposal description is category."}],
+      rules: {
+        title: [{required: true, trigger: 'blur', message: "Proposal title is category."}],
+        desc: [{required: true, trigger: 'blur', message: "Proposal description is category."}],
       },
       Governance: null,
       loading: false,
@@ -56,30 +60,34 @@ export default {
   watch: {
     web3Status: watch.web3Status,
   },
-  created(){
+  created() {
     this.initData();
-    this.$Bus.bindEvent(this.$EventNames.switchAccount, this._uid, (account)=>{
+    this.$Bus.bindEvent(this.$EventNames.switchAccount, this._uid, (account) => {
       this.initData();
     });
   },
   methods: {
-    initData(){
-      if(this.web3Status === this.WEB3_STATUS.AVAILABLE){
+    initData() {
+      if (this.web3Status === this.WEB3_STATUS.AVAILABLE) {
         this.initContract();
       }
     },
-    async initContract(){
-      if(!this.Governance) this.Governance = await this.getContract(GovernanceContract);
+    async initContract() {
+      if (!this.Governance) this.Governance = await this.getContract(GovernanceContract);
     },
-    createProposal(){
+    createProposal() {
       this.$refs.createFrm.validate(valid => {
-        if(valid){
+        if (valid) {
           this.loading = true;
           const instance = this.Governance.getContract().instance;
-          instance.createProposal(this.form.title, this.form.desc, "", 0, { from: this.member.account }).then(res => {
+          instance.createProposal(this.form.title, this.form.desc, "", 0, {from: this.member.account}).then(res => {
             console.info(res, res.toString());
             this.$message.success("Create proposal successfully");
             this.options.showCreate = false;
+            this.form = {
+              title: "",
+              desc: ""
+            }
             this.$emit("refresh");
           }).catch(e => {
             console.error(e);
@@ -95,9 +103,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import '@/styles/element-variables.scss';
-#gov-proposal-create{
-  .create-drawer{
+#gov-proposal-create {
+  .create-drawer {
     padding: 20px;
   }
 }
--- frontend/src/views/governance/proposalDetail.vue
+++ frontend/src/views/governance/proposalDetail.vue
@@ -3,12 +3,12 @@
     <el-card class="box-card">
       <div slot="header">
         <h2 class="main-text">
-          <highlight class="id secondary-text">#{{options.active.id}}</highlight>
-          <highlight class="normal-text">{{options.active.title}}</highlight>
+          <highlight class="id secondary-text">#{{options.activeProposal.id}}</highlight>
+          <highlight class="title normal-text">{{options.activeProposal.title}}</highlight>
         </h2>
       </div>
-      <ProposalDraft :options="options" v-if="options.active.status == 0" />
-      <ProposalAwaitingSolutions :options="options" v-else-if="options.active.status == 1" />
+      <ProposalDraft :options="options" v-if="options.activeProposal.status == 0" />
+      <ProposalAwaitingSolutions :options="options" v-else-if="options.activeProposal.status == 1" />
       <ProposalMemberVote :options="options" v-else-if="showVote()" />
       <ProposalVoteView :options="options" v-else-if="showVoteView()" />
     </el-card>
@@ -17,6 +17,7 @@
 
 <script>
 import { watch } from '@/utils/watch.js';
+import abi from 'ethereumjs-abi';
 import { mapGetters } from 'vuex';
 import ProposalDraft from './proposalDraft';
 import ProposalAwaitingSolutions from './proposalAwaitingSolutions';
@@ -46,7 +47,7 @@ export default {
   },
   watch: {
     web3Status: watch.web3Status,
-    "options.active": {
+    "options.activeProposal": {
       handler(newVal){
         if(newVal){
           this.initData();
@@ -72,11 +73,14 @@ export default {
       this.getSolutionAction();
     },
     getMemberVoteStatus(){
+      if (!this.member.account) {
+        return
+      }
       const instance = this.Governance.getContract().instance;
-      if(this.options.active && this.options.active.id){
-        instance.memberProposalVote(this.member.account, this.options.active.id).then(res => {
+      if(this.options.activeProposal && this.options.activeProposal.id){
+        instance.memberProposalVote(this.member.account, this.options.activeProposal.id).then(res => {
           console.info(res, res.toString());
-          this.options.active.voteId = res.toString();
+          this.options.activeProposal.voteId = res.toString();
           this.$forceUpdate();
         }).catch(e => {
           console.error(e);
@@ -85,27 +89,55 @@ export default {
       }
     },
     showVote(){
-      return this.options.active.status == 2 && (!this.options.active.voteId || parseInt(this.options.active.voteId) == 0);
+      return this.options.activeProposal.status == 2 && (!this.options.activeProposal.voteId || parseInt(this.options.activeProposal.voteId) == 0);
     },
     showVoteView(){
-      return parseInt(this.options.active.status) > 2 || (this.options.active.status == 2 && parseInt(this.options.active.voteId) > 0)
+      return parseInt(this.options.activeProposal.status) > 2 || (this.options.activeProposal.status == 2 && parseInt(this.options.activeProposal.voteId) > 0)
     },
     getSolutionAction(){
       const instance = this.Governance.getContract().instance;
-      const id = this.options.active.id;
-      if(this.options.active && this.options.active.id && this.options.active.status > 1){
+      const id = this.options.activeProposal.id;
+      if(this.options.activeProposal && this.options.activeProposal.id && this.options.activeProposal.status > 1){
         this.options.paramValues = [];
         instance.getSolutionAction(id, 1).then(res => {
-          // 目前所有治理参数都是两个，所以使用下面固定算法decode solution hash值，后期如过参数增加，需要修改此处代码
-          const solutionHash = res[1].toString();
-          const param1 = this.hexCharCodeToStr(solutionHash.substring(0, 34)).trim();
-          const param2 = this.hex2int(solutionHash.substring(34));
-          this.options.paramValues.push(param1);
-          this.options.paramValues.push(param2);
+          const action = res[1].toString();
+          let types = this.getParamsTypes()
+          const decoded = abi.rawDecode(types, Buffer.from(action.substr(2),'hex'));
+          this.addParams(types, decoded)
+          // const param1 = this.hexCharCodeToStr(action.substring(0, 34)).trim();
+          // const param2 = this.hex2int(action.substring(34));
+          // this.options.paramValues.push(param1);
+          // this.options.paramValues.push(param2);
         }).catch(e => {
           console.error(e);
           // this.$message.error(e.message);
         });
+      }
+    },
+    getParamsTypes() {
+      let category = this.options.activeProposal.category
+      for (let c of this.options.formatters.categories) {
+        if (c.index === category) {
+          return c.params.map(p => p.type)
+        }
+      }
+    },
+    addParams(types, params) {
+      for (let i = 0; i < types.length; i++) {
+        let type = types[i]
+        let param = params[i]
+        if (type.includes('[]')) {
+          // 数组暂时只有第一个元素
+          param = param[0]
+        }
+        if (type.includes('bytes')) {
+          param = new TextDecoder().decode(param)
+        } else if (type.includes('address')) {
+          param = '0x' + param
+        } else if (type.includes('uint')) {
+          param = param.toString()
+        }
+        this.options.paramValues.push(param);
       }
     },
     hex2int(hex) {
@@ -149,14 +181,22 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import '@/styles/element-variables.scss';
 #gov-proposal-detail{
   height: calc(100vh - 146px);
   .box-card{
+    display: flex;
+    flex-direction: column;
     height: calc(100%);
   }
   .id{
     margin-right: 10px;
+  }
+  .title {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
   }
 }
 </style>
@@ -164,7 +204,7 @@ export default {
 #gov-proposal-detail{
   .box-card{
     .el-card__body{
-      height: calc(100% - 110px);
+      //height: calc(100% - 110px);
       overflow-y: auto;
     }
   }
